1b34f9556b87d16b92a4e48c5d2a5065a32fab85
etoy22 <ethanleider@gmail.com>
2021-08-03 16:50:48 -0400

    Update

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import pandas as pd
 import cv2
 import numpy as np
 from sklearn import neighbors
+
+database = pd.read_csv("data.csv")
+database.head(15)
+
+#Need for describing the data as it is recieved
+database['Ethnicity'] = pd.to_numeric(database['Ethnicity'],errors='coerce')
+database.describe()
+
